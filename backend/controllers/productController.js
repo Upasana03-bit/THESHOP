2661e527ab9f2d75c00a888f158998164fec88d5
Upasana03 <upasanachoudhury2000@gmail.com>
2022-02-26 08:52:37 +0530

    new changes

---
+++ backend/controllers/productController.js
@@ -0,0 +1,31 @@
+import asyncHandler from "express-async-handler";
+
+import Product from "../models/productModel.js";
+
+
+
+
+// @desc    Fetch all Products
+// @route   Get /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+    const Products = await Product.find({});
+
+    res.json(Products);
+})
+
+// @desc    Fetch Single Product
+// @route   Get /api/product/:id
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+  
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product not Found!");
+    }
+})
+
+export { getProducts, getProductById }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+
 
 const App = () => {
   return (
@@ -13,8 +17,11 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
+        <Route path="/register" component={RegisterScreen} />
+        <Route path="/login" component={LoginScreen} />
         <Route exact path="/" component={HomeScreen} />
         <Route path="/product/:id" component={ProductScreen} />
+        <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
